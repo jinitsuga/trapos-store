f7f3037a43df096c436e8106494551bc403e37db
agu <iba.agustin@gmail.com>
2023-10-11 15:24:23 -0300

    add email magic link auth login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,47 @@
-import NextAuth from "next-auth/next";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 
-export const authOptions = {
-  providers: [EmailProvider({})],
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+
+import { MongoClient } from "mongodb";
+
+const {
+  MONGO_URI,
+  SMTP_HOST,
+  SMTP_PORT,
+  SMTP_USER,
+  SMTP_PASS,
+  SMTP_FROM,
+  ADMIN_EMAILS,
+} = process.env;
+
+const adminEmails = ADMIN_EMAILS?.split(",");
+
+const client = new MongoClient(MONGO_URI || "");
+const clientPromise = client.connect();
+
+const authOptions: NextAuthOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    EmailProvider({
+      server: {
+        host: SMTP_HOST,
+        port: SMTP_PORT,
+        auth: {
+          user: SMTP_USER,
+          pass: SMTP_PASS,
+        },
+      },
+      from: SMTP_FROM,
+    }),
+  ],
+  callbacks: {
+    signIn: ({ user }) =>
+      !!(user.email && adminEmails?.includes(user.email.toLowerCase())),
+  },
+  session: { strategy: "jwt" },
 };
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ req, token }) =>
+      token !== null ||
+      (!req.nextUrl.pathname.startsWith("/admin") &&
+        !req.nextUrl.pathname.startsWith("/api/uploadthing")),
+  },
+});
