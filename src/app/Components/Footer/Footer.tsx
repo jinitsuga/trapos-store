811fed0e3a5687b2403e28b5b996cc3d3ef1c6ee
agu <iba.agustin@gmail.com>
2023-10-11 21:25:04 -0300

    add uploadthing for product imgs, admin UI pending

--- src/app/Components/Footer/Footer.tsx
+++ src/app/Components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Navbar } from "./../Header/Navbar";
+import { Navbar } from "../Header/Navbar";
 
 export default function Footer() {
   return (
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <section>
+      <h3>Subir un producto</h3>
+    </section>
+  );
+}
