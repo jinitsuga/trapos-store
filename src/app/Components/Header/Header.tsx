b18205b47d329aa61087d613d425dd4ef36f0617
agu <iba.agustin@gmail.com>
2023-11-16 13:08:49 -0300

    add session provider to layout. Turn header component into client component

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 import { Navbar } from "./Navbar";
 import Image from "next/image";
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import { getServerSession } from "next-auth";
+import SessionProvider from "./Components/SessionProvider";
 import "@uploadthing/react/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,19 +14,22 @@ export const metadata: Metadata = {
   description: "La tienda de estampado y diseño que siempre buscaste.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
   return (
     <html lang="en">
       <body
         className={`${inter.className} min-h-screen bg-trapo-black text-trapo-green`}
       >
-        <Header />
-        {children}
-        <Footer />
+        <SessionProvider session={session}>
+          <Header />
+          {children}
+          <Footer />
+        </SessionProvider>
       </body>
     </html>
   );
