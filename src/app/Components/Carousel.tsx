172a14e30732635203c75bb7bf1e4f8272104a4f
agu <iba.agustin@gmail.com>
2023-11-17 17:11:02 -0300

    add img carousel functionality and hooked it on product page

--- src/app/Components/Carousel.tsx
+++ src/app/Components/Carousel.tsx
@@ -9,33 +9,45 @@ const changeImage = (
   currentImg: number,
   setter: Function
 ) => {
-  if (currentImg < images.length) {
-    setter(currentImg++);
+  if (currentImg < images.length - 1) {
+    setter(currentImg + 1);
   } else {
     setter(0);
   }
 };
 
-export default function Carousel(images: ImagesList) {
+export default function Carousel({ images }: { images: ImagesList }) {
   const [imageCount, setImageCount] = React.useState<number>(0);
+  console.log(imageCount);
 
+  console.log(images);
   return (
-    <div
-      className={`relative overflow-hidden rounded-[4.75%_/_3.5%] after:block after:pb-[140%] w-full
+    <div className="w-[400px] min-w-[300px] flex flex-col mr-10">
+      <div
+        className={`relative overflow-hidden rounded-[4.75%_/_3.5%] after:block after:pb-[140%] w-full
 }`}
-    >
-      <Image
-        className="absolute top-0 left-0 h-full w-full object-cover"
-        src={images[imageCount]}
-        fill
-        alt={"Photo of the product"}
-      />
-      <ul>
+      >
+        <Image
+          className="absolute top-0 left-0 h-full w-full object-cover"
+          src={images[imageCount]}
+          fill
+          alt={"Photo of the product"}
+        />
+      </div>
+      <ul className="flex w-full justify-around">
         <li>
-          <button>{"<"}</button>
+          <button className="h-8 w-8 text-white text-3xl">⇐</button>
         </li>
         <li>
-          <button>{">"}</button>
+          <button
+            className="h-8 w-8 text-white text-3xl"
+            onClick={() => {
+              console.log("clicked");
+              changeImage(images, imageCount, setImageCount);
+            }}
+          >
+            ⇒
+          </button>
         </li>
       </ul>
     </div>
--- src/app/tienda/[slug]/[id]/page.tsx
+++ src/app/tienda/[slug]/[id]/page.tsx
@@ -1,17 +1,18 @@
 import { getProduct } from "@/app/utils/product";
-import Image from "next/image";
+import Carousel from "@/app/Components/Carousel";
+import { Product } from "@/app/Components/Dashboard/ProductUploads";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
   const productData = await getProduct(id);
 
-  const { name, img, description, price } = productData;
-
-  // Add color and size selection buttons by mapping both arrays and rendering these btns.
+  const { name, img, description, price }: Product = productData;
+  console.log(img.length);
+  // Add color and size selection buttons by mapping both arrays and rendering as btns.
   return (
     <main className="flex flex-col w-full h-full items-center justify-center  p-2">
       <div className="flex mt-10 w-1/2  justify-center">
-        <div className="w-[300px] mr-10"></div>
+        <Carousel images={img} />
         <div className="text-gray-200 flex text-xl flex-col gap-4">
           <h3 className="text-3xl font-bold tracking-tight">{name}</h3>
           <p>{description}</p>
