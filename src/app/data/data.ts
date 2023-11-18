b749c1c14826814cc1ab4f76d6bc4dde7cea5e81
agu <iba.agustin@gmail.com>
2023-11-18 18:14:57 -0300

    add data file with colors, sizes and categories for future referencing

---
+++ src/app/data/data.ts
@@ -0,0 +1,13 @@
+import { Color } from "../Components/Dashboard/ProductUploads";
+
+export const categories = ["camisetas", "canguros", "gorras", "tazas", "todos"];
+
+export const sizes = ["XXL", "XL", "L", "M", "S"];
+
+export const colors: Color[] = [
+  { name: "azul", hex: "#1d4ed8" },
+  { name: "negro", hex: "#020617" },
+  { name: "gris", hex: "#4b5563" },
+  { name: "blanco", hex: "#fafafa" },
+  { name: "verde", hex: "#15803d" },
+];
