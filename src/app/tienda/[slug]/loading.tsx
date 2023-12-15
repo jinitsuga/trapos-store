14cf33fd4c03e1e456962955aab1e99bfd92a065
agu <iba.agustin@gmail.com>
2023-12-15 14:02:38 -0300

    add loading message for store categories

---
+++ src/app/tienda/[slug]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center text-xl text-white p-4">
+      <p>Cargando...</p>
+    </div>
+  );
+}
