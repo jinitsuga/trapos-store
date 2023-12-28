cbcb590b98415130e96569c99eb8a5db5788b1a3
agu <iba.agustin@gmail.com>
2023-12-28 16:52:24 -0300

    add option to upload form for tshirt subtype selection

--- src/app/Components/Dashboard/ProductUploads.tsx
+++ src/app/Components/Dashboard/ProductUploads.tsx
@@ -4,7 +4,8 @@ import { UploadButton } from "../../utils/uploadthing";
 import Image from "next/image";
 import { uploadProduct } from "../../utils/product";
 import Checkbox from "../Checkbox";
-import { colors, sizes, kidSizes } from "@/app/data/data";
+import { colors, sizes, kidSizes, tshirtTags } from "@/app/data/data";
+import { capitalize } from "@/app/utils/helpers";
 
 export type Color = {
   name: string;
@@ -131,6 +132,14 @@ export default function Uploads() {
     );
   });
 
+  const tagOptions = tshirtTags.map((tag, id) => {
+    return (
+      <option key={id} value={tag}>
+        {capitalize(tag)}
+      </option>
+    );
+  });
+
   const inputStyles =
     "text-black rounded p-2 focus:outline-none focus:ring focus:ring-green-700 mb-2";
 
@@ -190,102 +199,124 @@ export default function Uploads() {
             </>
           )}
           {/* If image is uploaded, ask if more images or load rest of form */}
-          {/* {product.img?.length! > 0 && imgDone && ( */}
-          <div className="flex text-xl mb-20 flex-col items-center justify-center gap-4">
-            <Image
-              width={150}
-              height={200}
-              className="h-auto"
-              src={product.img![0]}
-              alt="uploaded image"
-            ></Image>
-            <form className="flex flex-col items-cener justify-center">
-              <label htmlFor="name" className="mb-1">
-                Nombre del producto:
-              </label>
-              <input
-                onChange={(e) => {
-                  updateProduct(e);
-                }}
-                className="text-black rounded p-2 focus:outline-none focus:ring focus:ring-green-700 mb-2"
-                placeholder="Camiseta One Piece"
-                type="text"
-                name="name"
-              ></input>
-              <label htmlFor="price" className="mb-1">
-                Precio:
-              </label>
-              <input
-                onChange={(e) => {
-                  updateProduct(e);
-                }}
-                placeholder="400"
-                type="number"
-                name="price"
-                className={`${inputStyles} w-auto`}
-              ></input>
-              <label>Colores disponibles:</label>
-              <ul className="flex self-center gap-2 flex-wrap items-center justify-center max-w-[250px] mb-4">
-                {colorBoxes}
-              </ul>
-              <label>Tamaños disponibles:</label>
-              <ul className="flex self-center  items-center justify-center mb-4">
-                {sizeBoxes}
-              </ul>
-              <label>Tamaños de niños:</label>
-              <ul className="flex self-center flex-wrap items-center justify-center max-w-[200px]  mb-4">
-                {kidSizeBoxes}
-              </ul>
-              <label htmlFor="description" className="mb-1">
-                Descripción:
-              </label>
-              <textarea
-                onChange={(e) => {
-                  updateProduct(e);
-                }}
-                maxLength={120}
-                placeholder="Describe el producto brevemente"
-                className={`${inputStyles}`}
-                name="description"
-              ></textarea>
-              <label htmlFor="category" className="mb-1">
-                Categoría:
-              </label>
-              <select
-                onChange={(e) => {
-                  updateProduct(e);
-                }}
-                className={inputStyles}
-                name="type"
-              >
-                <option value="">--Elige categoría--</option>
-                <option value="camisetas">Camisetas</option>
-                <option value="canguros">Canguros</option>
-                <option value="gorras">Gorras</option>
-                <option value="tazas">Tazas</option>
-              </select>
-              {error && (
-                <span className="text-red-400">
-                  Asegurate de completar todos los campos
-                </span>
-              )}
-              <button
-                onClick={async (e) => {
-                  e.preventDefault();
-                  const validFields = validateFields();
-                  if (validFields) {
-                    await uploadProduct(product);
-                    setUploaded(true);
-                  }
-                }}
-                className="bg-white text-black text-center leading-5 text-2xl rounded w-[120px]
+          {product.img?.length! > 0 && imgDone && (
+            <div className="flex text-xl mb-20 flex-col items-center justify-center gap-4">
+              <Image
+                width={150}
+                height={200}
+                className="h-auto"
+                src={product.img![0]}
+                alt="uploaded image"
+              ></Image>
+              <form className="flex flex-col items-cener justify-center">
+                <label htmlFor="name" className="mb-1">
+                  Nombre del producto:
+                </label>
+                <input
+                  onChange={(e) => {
+                    updateProduct(e);
+                  }}
+                  className="text-black rounded p-2 focus:outline-none focus:ring focus:ring-green-700 mb-2"
+                  placeholder="Camiseta One Piece"
+                  type="text"
+                  name="name"
+                ></input>
+                <label htmlFor="price" className="mb-1">
+                  Precio:
+                </label>
+                <input
+                  onChange={(e) => {
+                    updateProduct(e);
+                  }}
+                  placeholder="400"
+                  type="number"
+                  name="price"
+                  className={`${inputStyles} w-auto`}
+                ></input>
+                <label>Colores disponibles:</label>
+                <ul className="flex self-center gap-2 flex-wrap items-center justify-center max-w-[250px] mb-4">
+                  {colorBoxes}
+                </ul>
+                <label>Tallas disponibles:</label>
+                <ul className="flex self-center  items-center justify-center mb-4">
+                  {sizeBoxes}
+                </ul>
+                <label>Tallas de niños disponibles:</label>
+                <ul className="flex self-center flex-wrap items-center justify-center max-w-[200px]  mb-4">
+                  {kidSizeBoxes}
+                </ul>
+                <label htmlFor="description" className="mb-1">
+                  Descripción:
+                </label>
+                <textarea
+                  onChange={(e) => {
+                    updateProduct(e);
+                  }}
+                  maxLength={120}
+                  placeholder="Describe el producto brevemente"
+                  className={`${inputStyles}`}
+                  name="description"
+                ></textarea>
+                <label htmlFor="category" className="mb-1">
+                  Categoría:
+                </label>
+                <select
+                  onChange={(e) => {
+                    updateProduct(e);
+                  }}
+                  className={inputStyles}
+                  name="type"
+                >
+                  <option value="">--Elige categoría--</option>
+                  <option value="camisetas">Camisetas</option>
+                  <option value="canguros">Canguros</option>
+                  <option value="gorras">Gorras</option>
+                  <option value="tazas">Tazas</option>
+                </select>
+                {product.type == "camisetas" && (
+                  <>
+                    <label htmlFor="category" className="mb-1">
+                      Tags de camisetas:
+                    </label>
+                    <select
+                      onChange={(e) => {
+                        updateProduct(e);
+                      }}
+                      className={inputStyles}
+                      name="subType"
+                    >
+                      {tshirtTags.map((tag, id) => {
+                        return (
+                          <option key={id} value={tag}>
+                            {capitalize(tag)}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </>
+                )}
+                {error && (
+                  <span className="text-red-400">
+                    Asegurate de completar todos los campos
+                  </span>
+                )}
+                <button
+                  onClick={async (e) => {
+                    e.preventDefault();
+                    const validFields = validateFields();
+                    if (validFields) {
+                      await uploadProduct(product);
+                      setUploaded(true);
+                    }
+                  }}
+                  className="bg-white text-black text-center leading-5 text-2xl rounded w-[120px]
                h-[40px] mt-4 self-center p-2 hover:bg-green-200 active:scale-105"
-              >
-                Agregar
-              </button>
-            </form>
-          </div>
-          {/* )} */}
+                >
+                  Agregar
+                </button>
+              </form>
+            </div>
+          )}
         </>
       ) : (
         <button
