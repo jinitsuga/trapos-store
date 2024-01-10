f4fb041397692c60baf0adbb600e97fa6630d491
agu <iba.agustin@gmail.com>
2024-01-09 22:30:30 -0300

    refactor db call for products inside server component. Calling directly using model instead of fetch method to API

--- src/app/tienda/[slug]/page.tsx
+++ src/app/tienda/[slug]/page.tsx
@@ -1,22 +1,25 @@
 import CategoriesNav from "@/app/Components/UI/CategoriesNav";
 import ProductsDisplay from "@/app/Components/UI/ProductsDisplay";
+import product from "@/app/models/product";
 
 async function getProducts(cat: string) {
-  const produs = await fetch(`${process.env.URL}/api/categories?cat=${cat}`, {
-    cache: "no-store",
-  });
-  if (!produs.ok) {
-    throw new Error("fetching data failed");
+  try {
+    let prods;
+    if (cat === "todos") {
+      prods = await product.find({}).lean().exec();
+    } else {
+      prods = await product.find({ type: cat }).lean().exec();
+    }
+    return { prods };
+  } catch (err) {
+    throw new Error("Fetching data failed");
   }
-  return produs.json();
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
   const fetchedProducts = await getProducts(slug);
-
-  const products = fetchedProducts.prods;
-
+  const products = fetchedProducts.prods as any;
   return (
     <main className="flex justify-center items-center mt-20 text-white">
       <div className="flex flex-col sm:flex-row w-full gap-2  sm:w-1/2">
